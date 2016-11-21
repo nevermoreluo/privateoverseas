8f1dad6cabe89aa9ef9f80442f65ad1cf396d4bb
nevermoreluo <nevermoreluo@gmail.com>
2016-11-21 23:18:00 +0800

    update admin

--- overseas/settings.py
+++ overseas/settings.py
@@ -225,7 +225,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Shanghai'
 
